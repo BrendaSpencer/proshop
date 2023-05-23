7898bebd655445b40c6194e1c60713b6cd2909c0
BrendaSpencer <spencerbernadette1@gmail.com>
2023-05-23 05:53:01 +0200

    custom error handling

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,4 +1,4 @@
-const notFound = ( req, res, next){
+const notFound = ( req, res, next) =>{
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
@@ -15,7 +15,7 @@ const errorHandler = (err, req, res, next) => {
 
     res.status(statusCode).json({
         message,
-        stack: ProcessingInstruction.env.NODE_ENV === 'production' ? 'stack' : err.stack,
+        stack: process.env.NODE_ENV === 'production' ? 'stack' : err.stack,
     })
 }
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,20 +4,26 @@ import Product from "../models/productModel.js";
 
 const router = express.Router();
 
-
-router.get('/',asyncHandler(async(req,res) => {
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
-}));
+  })
+);
 
-router.get('/:id',asyncHandler(async(req, res) => {
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
-    if(product){
-       return res.json(product);
+    if (product) {
+      return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
     }
-    res.status(404).json({ message: 'Product not found'})
-}));
-
+  })
+);
 
 export default router;
