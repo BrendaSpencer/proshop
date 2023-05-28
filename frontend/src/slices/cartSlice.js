ff687903dc3a69f0aa0880fe924cf001a1d186d5
BrendaSpencer <spencerbernadette1@gmail.com>
2023-05-28 09:36:30 +0200

    cart count header

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { updateCart} from "../utils/cartUtils";
 const initialState = localStorage.getItem('cart') ? JSON.parse
 (localStorage.getItem("cart")) : { cartItems: []};
 
-const addDecimals = (num) => {
-    return (Math.round(num * 100) / 100).toFixed(2);
-}
+
 const cartSlice = createSlice({
     name: "cart",
     initialState,
@@ -20,24 +18,10 @@ const cartSlice = createSlice({
             } else {
                 state.cartItems = [...state.cartItems, item]
             }
-            // Calculate items price
-            state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0))
 
-            // Calculate shipping price
-            state.shippingPrice = addDecimals(state.itemsPrice> 100 ? 0 : 10);
-
-            // Calculate tax price
-            state.taxPrice = addDecimals(Number( state.itemsPrice * 0.15).toFixed(2));
-
-            // Calculate total price
-            state.totalPrice = (
-                Number(state.itemsPrice) +
-                Number(state.shippingPrice) +
-                Number(state.taxPrice)
-            ).toFixed(2);
-
-            localStorage.setItem('cart', JSON.stringify(state))
+            return updateCart(state);
         }
+
     }
 })
 
