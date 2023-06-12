072fc44adcb41a045b9074b530fc7c833faec061
BrendaSpencer <spencerbernadette1@gmail.com>
2023-06-12 14:03:07 +0200

    delete users

--- frontend/src/screens/admin/UserListScreen.jsx
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -3,18 +3,32 @@ import { Table, Button } from "react-bootstrap";
 import { FaTrash, FaTimes, FaEdit, FaCheck } from "react-icons/fa";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
-import { useGetUsersQuery } from "../../slices/usersApiSlice";
+import { useGetUsersQuery, useDeleteUserMutation } from "../../slices/usersApiSlice";
+import {toast} from "react-toastify";
 
 const UserListScreen = () => {
   const { data: users,refetch, isLoading, error} = useGetUsersQuery();
-  
-  const deleteHandler = (id) => {
-    console.log('delete');
+
+  const [deleteUser, {isLoading: loadingDelete}] = useDeleteUserMutation();
+
+  const deleteHandler = async (id) => {
+    if(window.confirm('Are you sure?')){
+      try {
+        await deleteUser(id);
+        refetch();
+        toast.success('User deleted.')
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+
+        
+      }
+    }
   }
   
   return (
     <>
     <h1>Users</h1>
+    {loadingDelete && <Loader/>}
     {isLoading ? ( 
       <Loader />
     ): error ? (
